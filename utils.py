948baa34f975fb608b11870bf4ee5f91440a312e
Petar <7125535+sarajcev@users.noreply.github.com>
2026-05-19 08:37:51 +0200

    Add delta signals

--- utils.py
+++ utils.py
@@ -28,7 +28,7 @@ def create_white_list(prefix, sufixes, black):
     for sufix in sufixes:
         white_list.extend(
             [prefix + f'{i+1:02d}' + sufix 
-            for i in range(9) 
+            for i in range(10) 
             if i not in black]
         )
     return white_list
@@ -72,4 +72,70 @@ def plot_machine_signals(data, category):
     ax.set_ylabel(category)
     fig.tight_layout()
     plt.show()
+    return
+
+
+def plot_machine_delta_signals(data):
+    """
+    Plot EMTP-RV simulation signals for the machines.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Pandas DataFrame holding simulation signals.
+    
+    Returns
+    -------
+    Show matplotlib figure with plots of machine angles
+    with regards to the slack machine.
+
+    Notes
+    -----
+    This function is tailored for the machine signals,
+    which contain 'PowerPlant' string in their name.
+    DataFrame is exported from the simulation class.
+    """
+    time = data['time'].values
+    fig, ax = plt.subplots(figsize=(6.5, 3.5))
+    for name in data.columns:
+        string = re.search('Teta', name)
+        if string is not None:
+            signal = data[name].values
+            pp = name.split('/')[0][-2:]
+            if pp == '02':
+                # Slack machine.
+                slack_signal = signal
+                break
+    for name in data.columns:
+        string = re.search('Teta', name)
+        if string is not None:
+            signal = data[name].values
+            pp = name.split('/')[0][-2:]
+            if pp == '02':
+                continue
+            else:
+                ax.plot(time, slack_signal-signal, ls='-', lw=1.5, label=name.split('/')[0])
+    ax.legend(loc='upper left', frameon=True, fancybox=True, fontsize=8)
+    ax.grid(which='major', axis='both')
+    ax.set_xlabel('Time (s)')
+    fig.tight_layout()
+    plt.show()
+    return
+
+
+def plot_bus_voltages_rms(data, bus):
+    """
+    Plot three-phase bus voltage RMS values.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Pandas DataFrame holding simulation signals.
+    bus : int
+        Index of the bus.
+    
+    Returns
+    -------
+    Show matplotlib figure.
+    """
     return

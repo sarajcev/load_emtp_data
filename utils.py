ce99a5c178880d73fa679041e9bbc7ee7b6b2fa2
Petar <7125535+sarajcev@users.noreply.github.com>
2026-05-29 10:31:47 +0200

    Accommodate V0 variant.
    White list w/ and w/o renewables.

--- utils.py
+++ utils.py
@@ -2,22 +2,27 @@ import re
 import matplotlib.pylab as plt
 
 
-def create_white_list(varnames, exclude, buses):
+def create_white_list(varnames, exclude, buses, ibrs):
     """
-    Create a `white_list` variable for extracting
-    EMTP simulations signals.
+    Create a `white_list` variable for extracting 
+    EMTP simulations signals from the IEEE New England 
+    39-bus test power system.
 
     Parameters
     ----------
     varnames : list
-        List of machine variable names that will
-        be in the `white_list` variable.
+        List of machine variable names that will be in the 
+        `white_list` variable.
     exclude : list
-        List of indexes for the machines that are
-        excluded from the simulation.
+        List of indexes for the machines that are excluded from 
+        the simulation (and replaced by the renewable sources).
     buses : list
-        List of bus indexes that have voltage
-        measurements.
+        List of bus indexes that have voltage measurements.
+    ibrs : bool
+        Indicator variable (True/False) that specifies if the 
+        renewable sources are connected to the power system. 
+        Classical IEEE New England 39-bus power system has no 
+        connected renewables.
     
     Returns
     -------
@@ -47,19 +52,20 @@ def create_white_list(varnames, exclude, buses):
             if i not in exclude]
         )
 
-    # Wind farm signals (DEV2).
-    white_list.extend(['DEV2/P', 'DEV2/Q'])
-    white_list.extend(['DEV2/V0', 'DEV2/V1', 'DEV2/V2'])
-    white_list.extend(['DEV2/I0', 'DEV2/I1', 'DEV2/I2'])
-    white_list.append('FFC_WP2/Wind_Turbine/PMSG_T_rotor')
-    white_list.append('FFC_WP2/Wind_Turbine/PMSG_w_rotor')
-    white_list.append('FFC_WP2/Converter_control/Control/Grid_Ctrl/FRT_flag')
-
-    # Solar park signals (DEV3).
-    white_list.extend(['DEV3/P', 'DEV3/Q'])
-    white_list.extend(['DEV3/V0', 'DEV3/V1', 'DEV3/V2'])
-    white_list.extend(['DEV3/I0', 'DEV3/I1', 'DEV3/I2'])
-    white_list.append('WECC_PVPark_1/Converter_control/Control/GridControl_DLL/FRT_flag')
+    # Renewables connected to the power system?
+    if ibrs:
+        # Wind farm signals (DEV2).
+        white_list.extend(['DEV2/P', 'DEV2/Q'])
+        white_list.extend(['DEV2/V0', 'DEV2/V1', 'DEV2/V2'])
+        white_list.extend(['DEV2/I0', 'DEV2/I1', 'DEV2/I2'])
+        white_list.append('FFC_WP2/Wind_Turbine/PMSG_T_rotor')
+        white_list.append('FFC_WP2/Wind_Turbine/PMSG_w_rotor')
+        white_list.append('FFC_WP2/Converter_control/Control/Grid_Ctrl/FRT_flag')
+        # Solar park signals (DEV3).
+        white_list.extend(['DEV3/P', 'DEV3/Q'])
+        white_list.extend(['DEV3/V0', 'DEV3/V1', 'DEV3/V2'])
+        white_list.extend(['DEV3/I0', 'DEV3/I1', 'DEV3/I2'])
+        white_list.append('WECC_PVPark_1/Converter_control/Control/GridControl_DLL/FRT_flag')
     
     return white_list
 
